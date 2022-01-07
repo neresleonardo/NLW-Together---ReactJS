daf789dc5cfb2d484e0bb210f82e1fff329c996d
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-01-07 16:55:47 -0300

    finalizando admin

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
 import { NotFound } from './pages/NotFound';
+import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
@@ -16,7 +17,10 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
           <Route component={NotFound} />
+
+          
         </Switch>
 
 
